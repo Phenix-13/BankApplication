872b4219f11013c4babb752a887822d077e85f2c
Phenix-13 <alexis.aujard@gmail.com>
2024-05-15 15:27:52 +0200

    nuh uh

--- ms-loans/src/main/java/com/Phenix13/msloans/MsLoansApplication.java
+++ ms-loans/src/main/java/com/Phenix13/msloans/MsLoansApplication.java
@@ -2,8 +2,19 @@ package com.Phenix13.msloans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableFeignClients
+@RefreshScope
+@ComponentScans({@ComponentScan("com.Phenix13.msloans.controller")})
+@EnableJpaRepositories("com.Phenix13.msloans.repository")
+@EntityScan("com.Phenix13.msloans.model")
 public class MsLoansApplication {
 
 	public static void main(String[] args) {

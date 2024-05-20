e83829e18507b6e034a7a01c42ae625d6e791a74
Phenix-13 <alexis.aujard@gmail.com>
2024-05-20 12:03:44 +0200

    nuh uh

--- ms-account/src/main/java/com/Phenix13/msaccounts/controller/AccountController.java
+++ ms-account/src/main/java/com/Phenix13/msaccounts/controller/AccountController.java
@@ -4,6 +4,7 @@ import com.Phenix13.msaccounts.model.*;
 import com.Phenix13.msaccounts.repository.AccountRepository;
 import com.Phenix13.msaccounts.service.client.CardsFeignClient;
 import com.Phenix13.msaccounts.service.client.LoansFeignClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,6 +30,11 @@ public class AccountController {
         return accountRepository.getAccountByCustomerId(customer.getCustomerId());
     }
 
+    //@CircuitBreaker(name = "detailsForCustomerSupportApp")
+    @CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallBack")
+
+
+
     @PostMapping("/myCustomerDetails")
     public CustomerDetails myCustomerDetails(@RequestBody Customer customer){
         Accounts account = accountRepository.findByCustomerId(customer.getCustomerId());
@@ -41,6 +47,14 @@ public class AccountController {
         customerDetails.setCards(cards);
 
         return customerDetails;
+    }
 
+    private CustomerDetails myCustomerDetailsFallBack(Customer customer, Throwable t){
+        Accounts accounts = accountRepository.findByCustomerId(customer.getCustomerId());
+        List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+        CustomerDetails customerDetails = new CustomerDetails();
+        customerDetails.setAccounts(accounts);
+        customerDetails.setLoans(loans);
+        return customerDetails;
     }
 }
